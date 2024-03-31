99a71ec7e5dd50e586093d8483c5e69a9fc90b2f
AlexZibin <alexander.zibin.spb@yandex.ru>
2024-04-01 00:51:55 +0300

    Полностью переработал все отступы )

--- app/src/main/java/com/practicum/playlistmaker2/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker2/SettingsActivity.kt
@@ -1,21 +1,16 @@
 package com.practicum.playlistmaker2
 
-import android.app.Activity
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-     /*   findViewById<Button>(R.id.arrow_back).setOnClickListener {
-            val intent = Intent()
-            intent.putExtra("MainActivity", true)
-            setResult(Activity.RESULT_OK, intent)
-            finish() // Испробовал все варианты - всё равно приложение вылетает, если раскомментировать этот listener (((
-        }*/
+        findViewById<ImageView>(R.id.arrow_back).setOnClickListener { //<ImageView> !!!
+            finish()
+        }
     }
 }
